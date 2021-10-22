fc313b1c53abb57845d7cba3fde97da91769b21e
Hans <sweatermate@gmail.com>
2021-10-22 10:52:50 +0800

    Update to include HelloWorld.java

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -2,6 +2,8 @@ public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello");
         System.out.println("Testing");
+        Test abc = new Test();
+        abc.buy(1,2);
 
     }
 }
---
+++ src/Test.java
@@ -0,0 +1,11 @@
+public class Test {
+    private int shares;
+    private int cars;
+    private int total;
+    public void buy(int s,int c){
+        total = s + c;
+        System.out.println(total);
+
+    }
+
+}
